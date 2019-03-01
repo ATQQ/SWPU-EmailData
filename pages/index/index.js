4010b32900928f1dc5aa6b36f3fb142a7e07fc55
ATQQ <2604395430@qq.com>
2019-03-01 10:14:15 +0800

    完成登录页面构建

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,27 @@ const app = getApp()
 
 Page({
   data: {
-    
+    account:'',
+    password:''
   },
+  // 输入事件
+  accountInput:function (e) {
+    this.setData({
+      account:e.detail
+    })
+  },
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail
+    })
+  },
+  // 登录
+  btnLogin:function () {
+    console.log(this.data.account);
+    console.log(this.data.password);
+
+  },
+
   onLoad: function () {
   
   }
